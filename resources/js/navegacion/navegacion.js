8258dac0dad3cf5573c5a628172357009b31ed4d
Giussepe Delgado <giussepe@latamready.com>
2023-06-17 05:13:18 -0500

    Navegacion terminada

--- resources/js/navegacion/navegacion.js
+++ resources/js/navegacion/navegacion.js
@@ -20,15 +20,55 @@
 
 
 var iconMenu = document.querySelector('.menu-burguer');
+const bodyItems = document.querySelector('.items');
+const navCuerpo = document.getElementById('nacCuerpo');
+const icons = document.querySelectorAll('.iconOption');
+const olaSup = document.querySelector('.ola_superior__url');
+const olaInfo = document.querySelector('.ola_inferior__url');
+const logoPucu = document.querySelector('.logo-pucu');
+iconMenu.addEventListener('click', function () {
+    if (iconMenu.classList.contains('open')) {
+        iconMenu.classList.remove('open');
+    } else {
+        iconMenu.classList.add('open');
+    }
 
-iconMenu.addEventListener('click', function() {
-  if (iconMenu.classList.contains('open')) {
-    iconMenu.classList.remove('open');
-  } else {
-    iconMenu.classList.add('open');
-  }
+    if (navCuerpo.classList.contains('active-complex')) {
+        //incomplex
+        olaSup.classList.remove('active-complex');
+        olaInfo.classList.remove('active-complex');
+        items.forEach(item => {
+            const spanElement = item.querySelector('span.item__nombre');
+            spanElement.classList.remove('active-complex');
+            item.classList.remove('active-complex');
+        });
+
+        icons.forEach(icon =>{
+            icon.classList.remove('active-complex')
+        })
+
+        bodyItems.classList.remove('active-complex');
+        navCuerpo.classList.remove('active-complex');
+        logoPucu.classList.remove('active-complex');
+    } else {
+        //complex
+        olaSup.classList.add('active-complex');
+        olaInfo.classList.add('active-complex');
+        items.forEach(item => {
+            const spanElement = item.querySelector('span.item__nombre');
+            spanElement.classList.add('active-complex');
+            item.classList.add('active-complex');
+        });
+
+        icons.forEach(icon =>{
+            icon.classList.add('active-complex')
+        })
+
+        bodyItems.classList.add('active-complex');
+        navCuerpo.classList.add('active-complex');
+        logoPucu.classList.add('active-complex');
+    }
 }, false);
-
 
 
 const items = document.querySelectorAll('.item');
@@ -202,6 +242,10 @@ if (title === 'Orden de pago') {
     const container = document.querySelector('.container-pagos');
     container.classList.remove('desactive');
 }
+
+
+
+
 
 
 
--- resources/views/navegacion/panelnav.blade.php
+++ resources/views/navegacion/panelnav.blade.php
@@ -11,76 +11,73 @@
 <body>
     <div class="fondo">
         <div class="cuerpo">
-            <nav class="nav_cuerpo">
+            <nav id="nacCuerpo" class="nav_cuerpo">
+                <div class="ola_superior">
+                    <img class="ola_superior__url" src="{{ asset('storage') . '/' . 'uploads/navegacion/olasuperior.svg' }}">
+                    <div class="logo-pucu">
+                        <div class="logo-pucu__url"
+                            style="background-image: url({{ asset('storage') . '/' . 'uploads/navegacion/escudo.svg' }});">
+                        </div>
+                    </div>
+                </div>
 
-                <img class="ola_superior" src="{{ asset('storage') . '/' . 'uploads/navegacion/olasuperior.svg' }}">
 
                 <div class="items">
-                    <div id="itemUsuario" class="item">
-                        <div class="item__icono"
-                            style="background-image: url({{ asset('storage') . '/' . 'uploads/navegacion/icono_usuarios.svg' }});">
-                        </div>
-                        <div class="item__icono--blanco disable"
-                            style="background-image: url({{ asset('storage') . '/' . 'uploads/navegacion/icono_usuarios_blanco.svg' }});">
-                        </div>
-                        {{-- <i class="fa-regular fa-user"></i> --}}
-                        <span class="item__nombre">Usuarios</span>
-                    </div>
 
                     <div id="itemConsulta" class="item">
 
-                        <div class="item__icono"
+                        <div class="item__icono iconOption"
                             style="background-image: url({{ asset('storage') . '/' . 'uploads/navegacion/icono_consulta.svg' }});">
                         </div>
-                        <div class="item__icono--blanco disable"
+                        <div class="item__icono--blanco disable iconOption"
                             style="background-image: url({{ asset('storage') . '/' . 'uploads/navegacion/icono_consulta_blanco.svg' }});">
                         </div>
                         <span class="item__nombre">Consulta Vehícular</span>
                     </div>
 
                     <div id="itemReporte" class="item">
-                        <div class="item__icono"
+                        <div class="item__icono iconOption"
                             style="background-image: url({{ asset('storage') . '/' . 'uploads/navegacion/icono_reporte.svg' }});">
                         </div>
-                        <div class="item__icono--blanco disable"
+                        <div class="item__icono--blanco disable iconOption"
                             style="background-image: url({{ asset('storage') . '/' . 'uploads/navegacion/icono_reporte_blanco.svg' }});">
                         </div>
                         <span class="item__nombre">Reporte Laborales</span>
                     </div>
 
                     <div id="itemPapeleta" class="item">
-                        <div class="item__icono"
+                        <div class="item__icono iconOption"
                             style="background-image: url({{ asset('storage') . '/' . 'uploads/navegacion/icono_papeletas.svg' }});">
                         </div>
-                        <div class="item__icono--blanco disable"
+                        <div class="item__icono--blanco disable iconOption"
                             style="background-image: url({{ asset('storage') . '/' . 'uploads/navegacion/icono_papeletas_blanco.svg' }});">
                         </div>
                         <span class="item__nombre">Papeletas</span>
                     </div>
 
                     <div id="itemPago" class="item">
-                        <div class="item__icono"
+                        <div class="item__icono iconOption"
                             style="background-image: url({{ asset('storage') . '/' . 'uploads/navegacion/icono_ordenpago.svg' }});">
                         </div>
-                        <div class="item__icono--blanco disable"
+                        <div class="item__icono--blanco disable iconOption"
                             style="background-image: url({{ asset('storage') . '/' . 'uploads/navegacion/icono_ordenpago_blanco.svg' }});">
                         </div>
                         <span class="item__nombre">Orden de Pago</span>
                     </div>
                     <a class="item" href="{{ route('modulos') }}">
-                        <div class="item__icono"
+                        <div class="item__icono iconOption"
                             style="background-image: url({{ asset('storage') . '/' . 'uploads/navegacion/icono_modulos.svg' }});">
                         </div>
-                        <div class="item__icono--blanco disable"
+                        <div class="item__icono--blanco disable iconOption"
                             style="background-image: url({{ asset('storage') . '/' . 'uploads/navegacion/icono_modulos_blanco.svg' }});">
                         </div>
                         <span class="item__nombre">Modulos</span>
                     </a>
                     <a class="item" href="{{ route('login') }}">
-                        <div class="item__icono"
+                        <div class="item__icono iconOption"
                             style="background-image: url({{ asset('storage') . '/' . 'uploads/navegacion/icono_cerrarsesion.svg' }});">
                         </div>
-                        <div class="item__icono--blanco disable"
+                        <div class="item__icono--blanco disable iconOption"
                             style="background-image: url({{ asset('storage') . '/' . 'uploads/navegacion/icono_cerrarsesion_blanco.svg' }});">
                         </div>
                         <span class="item__nombre">Cerrar Sesión</span>
@@ -88,13 +85,15 @@
 
 
                 </div>
+                <div class="ola_inferior">
+                    <img class="ola_inferior__url" src="{{ asset('storage') . '/' . 'uploads/navegacion/olainferior.svg' }}">
+                </div>
 
-                <img class="ola_inferior" src="{{ asset('storage') . '/' . 'uploads/navegacion/olainferior.svg' }}">
 
             </nav>
             <div class="seccion">
                 <div class="barra">
-                    <div class="burguer-icon">
+                    <div id="burguerIcon" class="burguer-icon">
                         <div class="menu-burguer">
                             <div class="menu-line"></div>
                             <div class="menu-line"></div>
