20c133a7a307246e6cf5400f942e2994e181b92a
Giussepe Delgado <giussepe@latamready.com>
2023-06-21 02:53:27 -0500

    Formulario counsuklta vehicular asi terminado

--- resources/views/transporte/navegacion/nav_transporte.blade.php
+++ resources/views/transporte/navegacion/nav_transporte.blade.php
@@ -3,7 +3,7 @@
 @section('head')
     <!-- En esta seccion se pondran los links de losa rchivos css-->
     <title>{{$title}}</title>
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 @endsection
 
 @section('itemDinamic')
@@ -51,7 +51,7 @@
 
 @section('userFoto')
     <div class="foto__url"
-        style="background-image: url({{ asset('storage') . '/' . 'uploads/navegacion/fotouser.svg' }});">
+        style="background-image: url({{ asset('storage') . '/' . 'uploads/navegacion/fotouser.jpeg' }});">
     </div>
 @endsection
 
@@ -69,6 +69,8 @@
 @endsection
 
 @section('footer')
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     @vite(['resources/scss/templates/main-templates.scss','resources/js/transporte/nav_transporte.js'])
     @vite(['resources/scss/transporte/pagos/main-pagos.scss',
     'resources/scss/transporte/reportes/main-reportes.scss',
--- resources/views/transporte/vehiculo/registro.blade.php
+++ resources/views/transporte/vehiculo/registro.blade.php
@@ -1,55 +1,102 @@
-<!DOCTYPE html>
+<div class="registro registro--desactive">
 
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <!--<title>Registration Form in HTML CSS</title>-->
-    <!---Custom CSS File--->
-    <link rel="stylesheet" href="style.css" />
-</head>
+    <div class="container">
+        <div class="title-registro-vhicular">
+            <h1>Registro vehicular</h1>
+        </div>
 
-<body>
-    <div class="registro registro--desactive">
-        <h1>Registro</h1>
-        <body>
-            <section class="container">
-                <header>Registro</header>
-                <cajon action="#" class="form">
-                    <div class="input-box">
-                        <label>Empresa ó Asociación</label>
-                        <input type="text" placeholder="Ingresel nombre completo de la empresa" required />
-                    </div>
-                    <div class="column">
-                        <div class="input-box">
-                            <label>Estado de Inspección</label>
-                            <input type="text" placeholder="Aprobado, Desaprobado, observacion" required />
-                        </div>
-                        <div class="input-box">
-                            <label>Fecha</label>
-                            <input type="date" placeholder="Enter birth date" required />
-                        </div>
-                    </div>
-                    <div class="input-box address">
-                        <div class="column">
-                            <div class="select-box">
-                                <select>
-                                    <option hidden>Country</option>
-                                    <option>Aprobado</option>
-                                    <option>Denegado</option>
-                                    <option>En Proceso</option>
-                                    <option>Observado</option>
-                                </select>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="btnregistrar">
-                        <btn>Registrar</btn>
-                    </div>
+        <form method="POST" action="{{ route('vehiculos.store') }}">
+            @csrf
 
-                </cajon>
-            </section>
+            <div class="campo-tercio">
+                <label for="numero_municipal" class="form-label">Número Municipal</label>
+                <input type="text" name="numero_municipal" id="numero_municipal" class="form-control" required>
+            </div>
+            <div class="campo-tercio">
+                <label for="empresa" class="form-label">Empresa</label>
+                <input type="text" name="empresa" id="empresa" class="form-control" required>
+            </div>
+
+            <div class="campo-tercio">
+                <label for="placa" class="form-label">Placa</label>
+                <input type="text" name="placa" id="placa" class="form-control" required>
+            </div>
+
+            <div class="campo-dos-tercio">
+                <label for="propietario" class="form-label">Propietario</label>
+                <input type="text" name="name_propietario" id="name_propietario" value="JORGE ZEGARRA VALIENTE" class="form-control" required>
+                <input type="text" name="id_propietario" id="id_propietario" class="oculto" >
+                <div class="grupo-btn">
+                    <div class="btn-buscar">
+
+                    </div>
+                    <div class="btn-agregar">
+
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="campo-dos-tercio">
+                <label for="chofer" class="form-label">Chofer</label>
+                <input type="text" name="chofer" id="chofer" class="form-control" required>
+                <input type="text" name="id_chofer" id="id_chofer" class="oculto" >
+            </div>
+
+            <div class="campo-mitad">
+                <label for="estado_inspeccion" class="form-label">Estado Inspección</label>
+                <select id="estado_inspeccion" class="form-select">
+                    <option selected value="Aprobado">Aprobado</option>
+                    <option value="Desaprabado">Desprabado</option>
+                    <option value="Observado">Observado</option>
+                    <option value="NN">N.N</option>
+                </select>
+            </div>
+
+            <div class="campo-mitad">
+                <label for="fecha_inspeccion" class="form-label">Fecha Inspección</label>
+                <input type="date" name="fecha_inspeccion" id="fecha_inspeccion" class="form-control" required>
+            </div>
+
+
+
+            <div class="campo-mitad">
+                <label for="estado_credencial" class="form-label">Estado Credencial</label>
+                <select id="estado_credencial" class="form-select">
+                    <option selected value="Entregado">Entregado</option>
+                    <option value="Ninguno">Ninguno</option>
+                    <option value="En proceso">En proceso</option>
+
+                </select>
+            </div>
+
+            <div class="campo-mitad">
+                <label for="fecha_credencial" class="form-label">Fecha Credencial</label>
+                <input type="date" name="fecha_credencial" id="fecha_credencial" class="form-control" required>
+            </div>
+
+            <div class="campo-mitad">
+                <label for="curso_vial" class="form-label">Charla de seguridad vial</label>
+                <select id="curso_vial" class="form-select">
+                    <option selected value="Asistio">Asistio</option>
+                    <option value="No recibio">No recibio</option>
+                    <option value="En programacion">En programacion</option>
+                </select>
+            </div>
+
+            <div class="campo-mitad">
+                <label for="estado" class="form-label">Estado</label>
+                <select id="estado" class="form-select">
+                    <option selected value="Habilitado">Habilitado</option>
+                    <option value="No Habilitado">No Habilitado</option>
+                    <option value="En proceso">En proceso</option>
+                </select>
+            </div>
+
+            <div class="div-btn-guardar">
+                <button type="submit" class="btn btn-primary btn-guardar">Guardar</button>
+            </div>
+        </form>
+
     </div>
-</body>
-
-</html>
+</div>
