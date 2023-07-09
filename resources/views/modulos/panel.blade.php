57ec2de70224f84edd418dc0060dd0f66c9881b1
Hilton Bill Caballero <hiltonbillcaballero@gmail.com>
2023-07-09 17:26:35 -0500

    modificacion imagenes y iconos Modulos

--- resources/views/modulos/panel.blade.php
+++ resources/views/modulos/panel.blade.php
@@ -57,105 +57,89 @@
     <div class="tarjetas">
 
         @include('modulos.layouts.tarjeta', [
-            'imagen' => asset('storage') . '/' . 'uploads/modulos/transporte.svg',
-            'title' => '<p>TRANSPORTE</p>',
-            'icono' => asset('storage') . '/' . 'uploads/modulos/icono_transporte.svg',
-            'descripcion' => '<p>Gerencia de Seguridad Ciudadana, Fizcalización y Control</p>',
-            'route' => route('vehiculo.consulta')
-        ])
-
-        @include('modulos.layouts.tarjeta', [
-            'imagen' => asset('storage') . '/' . 'uploads/modulos/seguridad.svg',
-            'title' => '<P>SEGURIDAD CIUDADANA</p>',
-            'icono' => asset('storage') . '/' . 'uploads/modulos/icono_seguridad_ciudadana.svg',
-            'descripcion' => '<p>Gerencia de Seguridad Ciudadana, Fizcalización y Control</p>',
-            'route' => route('vehiculo.consulta')
-        ])
-
-        @include('modulos.layouts.tarjeta', [
-            'imagen' => asset('storage') . '/' . 'uploads/modulos/gestion_desastres.svg',
-            'title' => '<p>GESTIÓN Y RIESGOS</p><p>DE DESASTRES</p>',
-            'icono' => asset('storage') . '/' . 'uploads/modulos/icono_gestion_riesgo.svg',
-            'descripcion' => '<p>Gerencia de Desarrollo Económico y Turismo</p>',
-            'route' => route('vehiculo.consulta')
-        ])
-
-        @include('modulos.layouts.tarjeta', [
-            'imagen' => asset('storage') . '/' . 'uploads/modulos/fiscalizacion.svg',
-            'title' => '<p>FISCALIZACIÓN Y CONTROL</p>',
-            'icono' => asset('storage') . '/' . 'uploads/modulos/icono_fiscalizacionycontrol.svg',
-            'descripcion' => '<p>Gerencia de Seguridad Ciudadana, Fizcalización y Control</p>',
-            'route' => route('vehiculo.consulta')
-        ])
-
-        @include('modulos.layouts.tarjeta', [
-            'imagen' => asset('storage') . '/' . 'uploads/modulos/turismo.svg',
-            'title' => '<p>DESARROLLO ECONÓMICO</p><p>Y TURISMO</p>',
-            'icono' => asset('storage') . '/' . 'uploads/modulos/icono_turismo.svg',
-            'descripcion' => '<p>Gerencia de Desarrollo Económico y Turismo</p>',
-            'route' => route('vehiculo.consulta')
-        ])
-
-        @include('modulos.layouts.tarjeta', [
-            'imagen' => asset('storage') . '/' . 'uploads/modulos/salud_sanidad.svg',
-            'title' => '<p>SANIDAD Y SALUD</p>',
-            'icono' => asset('storage') . '/' . 'uploads/modulos/icono_saludysanidad.svg',
-            'descripcion' => '<p>Gerencia de Desarrollo Humano y Social</p>',
-            'route' => route('vehiculo.consulta')
-        ])
-
-        @include('modulos.layouts.tarjeta', [
-            'imagen' => asset('storage') . '/' . 'uploads/modulos/desarrollo_social.svg',
-            'title' => '<p>DESARROLLO SOCIAL, DEMUNA, OMAPED, CIAM</p>',
-            'icono' => asset('storage') . '/' . 'uploads/modulos/icono_desarrollosocial.svg',
-            'descripcion' => '<p>Gerencia de Desarrollo Humano y Social</p>',
-            'route' => route('vehiculo.consulta')
-        ])
-
-        @include('modulos.layouts.tarjeta', [
-            'imagen' => asset('storage') . '/' . 'uploads/modulos/contabilidad.svg',
-            'title' => '<p>CONTABILIDAD</p>',
-            'icono' => asset('storage') . '/' . 'uploads/modulos/icono_contabilidad.svg',
-            'descripcion' => '<p>Gerencia de Administración y Finanzas</p>',
-            'route' => route('vehiculo.consulta')
-        ])
-
-        @include('modulos.layouts.tarjeta', [
-            'imagen' => asset('storage') . '/' . 'uploads/modulos/sistemas.svg',
-            'title' => '<p>TEC. DE LA INFORMACIÓN Y</p><p>SISTEMAS</p>',
-            'icono' => asset('storage') . '/' . 'uploads/modulos/icono_sistemas.svg',
-            'descripcion' => '<p>Gerencia Municipal</p>',
+            'imagen' => asset('storage') . '/' . 'uploads/modulos/administracion_finanzas.svg',
+            'title' => '<p>ADMINISTRACIÓN Y FINANZAS</p>',
+            'icono' => asset('storage') . '/' . 'uploads/modulos/icono_finanzas.svg',
+            'descripcion' => '<p>Recursos Humanos, Tesorería, Logística y Control Patrimonial</p>',
             'route' => route('vehiculo.consulta')
         ])
 
         @include('modulos.layouts.tarjeta', [
             'imagen' => asset('storage') . '/' . 'uploads/modulos/administracion_tributaria.svg',
-            'title' => '<p>ADMINISTRACIÓN TRIBUTARIA</p>',
+            'title' => '<P>ADMINISTRACIÓN TRIBUTARIA</p>',
             'icono' => asset('storage') . '/' . 'uploads/modulos/icono_administracion_tributaria.svg',
-            'descripcion' => '<p>Gerencia de Administración y Finanzas</p>',
+            'descripcion' => '<p>Recaudación y Ejecutora Coactiva, Fiscalización Tributaria</p>',
             'route' => route('vehiculo.consulta')
         ])
 
         @include('modulos.layouts.tarjeta', [
-            'imagen' => asset('storage') . '/' . 'uploads/modulos/tesoreria.svg',
-            'title' => '<p>TESORERIA</p>',
-            'icono' => asset('storage') . '/' . 'uploads/modulos/icono_tesoreria.svg',
-            'descripcion' => '<p>Gerencia de Administración y Finanzas</p>',
+            'imagen' => asset('storage') . '/' . 'uploads/modulos/asesoria_juridica.svg',
+            'title' => '<p>ASESORÍA JURÍDICA</p>',
+            'icono' => asset('storage') . '/' . 'uploads/modulos/icono_juridica.svg',
+            'descripcion' => '<p>Asesoría Jurídica</p>',
             'route' => route('vehiculo.consulta')
         ])
 
         @include('modulos.layouts.tarjeta', [
-            'imagen' => asset('storage') . '/' . 'uploads/modulos/fizcalizacion_tributaria.svg',
-            'title' => '<p>FIZCALIZACIÓN</p><p>TRIBUTARIA</p>',
-            'icono' => asset('storage') . '/' . 'uploads/modulos/icono_fiscalizacion_tributaria.svg',
-            'descripcion' => '<p>Gerencia de Administración Tributaria</p>',
+            'imagen' => asset('storage') . '/' . 'uploads/modulos/planeamiento_presupuesto.svg',
+            'title' => '<p>PLANEAMIENTO Y PRESUPUESTO</p>',
+            'icono' => asset('storage') . '/' . 'uploads/modulos/icono_planeamiento.svg',
+            'descripcion' => '<p>Planeamiento y Presupuesto</p>',
+            'route' => route('vehiculo.consulta')
+        ])
+
+        @include('modulos.layouts.tarjeta', [
+            'imagen' => asset('storage') . '/' . 'uploads/modulos/desarrollo_urbano.svg',
+            'title' => '<p>DESARROLLO URBANO</p>',
+            'icono' => asset('storage') . '/' . 'uploads/modulos/icono_durbano.svg',
+            'descripcion' => '<p>Obras Públicas, Estudios y Proyectos, Obras Privadas y Catastro</p>',
+            'route' => route('vehiculo.consulta')
+        ])
+
+        @include('modulos.layouts.tarjeta', [
+            'imagen' => asset('storage') . '/' . 'uploads/modulos/economico_turismo.svg',
+            'title' => '<p>DESARROLLO ECONÓMICO Y TURISMO</p>',
+            'icono' => asset('storage') . '/' . 'uploads/modulos/icono_turismo.svg',
+            'descripcion' => '<p>Comercialización, Gestión del Riesgo de Desastres</p>',
+            'route' => route('vehiculo.consulta')
+        ])
+
+        @include('modulos.layouts.tarjeta', [
+            'imagen' => asset('storage') . '/' . 'uploads/modulos/desarrollo_social.svg',
+            'title' => '<p>DESARROLLO HUMANO SOCIAL</p>',
+            'icono' => asset('storage') . '/' . 'uploads/modulos/icono_desarrollo_social.svg',
+            'descripcion' => '<p>Participación Vecinal, Salud y Sanidad, Desarrollo Social, DEMUNA, OMAPED, CIAM Y Mujer </p>',
+            'route' => route('vehiculo.consulta')
+        ])
+
+        @include('modulos.layouts.tarjeta', [
+            'imagen' => asset('storage') . '/' . 'uploads/modulos/gestion_ambiental.svg',
+            'title' => '<p>SERVICIOS A LA CIUDAD Y GESTION AMBIENTAL</p>',
+            'icono' => asset('storage') . '/' . 'uploads/modulos/icono_ambiental.svg',
+            'descripcion' => '<p>Servicios a la Ciudad y Gestión Ambiental</p>',
+            'route' => route('vehiculo.consulta')
+        ])
+
+        @include('modulos.layouts.tarjeta', [
+            'imagen' => asset('storage') . '/' . 'uploads/modulos/seguridad.svg',
+            'title' => '<p>SEGURIDAD CIUDADANA, FISCALIZACIÓN Y CONTROL</p>',
+            'icono' => asset('storage') . '/' . 'uploads/modulos/icono_seguridad_ciudadana.svg',
+            'descripcion' => '<p>Seguridad Ciudadana, Fiscalización y Control y Transporte</p>',
+            'route' => route('vehiculo.consulta')
+        ])
+
+        @include('modulos.layouts.tarjeta', [
+            'imagen' => asset('storage') . '/' . 'uploads/modulos/sistemas.svg',
+            'title' => '<p>TÉCNOLOGIA DE LA INFORMACIÓN Y SISTEMAS</p>',
+            'icono' => asset('storage') . '/' . 'uploads/modulos/icono_sistemas.svg',
+            'descripcion' => '<p>Técnologia de la Información y Sistemas</p>',
             'route' => route('vehiculo.consulta')
         ])
 
         @include('modulos.layouts.tarjeta', [
             'imagen' => asset('storage') . '/' . 'uploads/modulos/moduloregistrousuario.svg',
             'title' => '<p>USUARIOS</p>',
-            'icono' => asset('storage') . '/' . 'uploads/modulos/sinicono.svg',
+            'icono' => asset('storage') . '/' . 'uploads/modulos/icono-usuario.svg',
             'descripcion' => '<p>Usuarios</p>',
             'route' => route('auth.lista')
         ])
